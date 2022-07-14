47239e2aff8b5c48b00792da28cea1b2595d6e42
Roman Titov <107221825+romantitovmephi@users.noreply.github.com>
2022-07-15 00:52:40 +0300

    Add files via upload

---
+++ basic modules/patternMealy.sv
@@ -0,0 +1,27 @@
+module patternMealy(input logic clk, 
+                    input logic reset,
+                    input logic a,
+                    output logic y); 
+  typedef enum logic [1:0] {S0, S1}
+statetype;
+  statetype state, nextstate;
+  
+// регистр состояния 
+always_ff @(posedge clk,
+            posedge reset) 
+		if (reset) state <= S0;
+      else state <= nextstate;
+		
+// логика следующего состояния 
+always_comb
+  case (state)
+S0: if (a) nextstate = S0;
+    else   nextstate = S1;
+S1: if (a) nextstate = S0;
+    else   nextstate = S1;
+ default:  nextstate = S0;
+endcase
+
+// выходная логика
+assign y = (a & state == S1); 
+endmodule
